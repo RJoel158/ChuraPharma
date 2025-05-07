f44c5fd45955bdb3b3c3eaa0026ab6e903ad947e
RJoel158 <svr0035567@est.univalle.edu>
2025-05-07 15:02:07 -0400

    style index

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $productos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <body class="bg-light">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
         <div class="container">
-            <a class="navbar-brand" href="#">Mi Tienda</a>
+            <a class="navbar-brand" href="#">ChuraPharma</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
